73d6435dedb339df3fd8614be5331e085824bdc0
gardianos <gardianii@hotmail.com>
2021-12-28 01:01:38 +0100

    post routes controller model

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ import dotenv from "dotenv";
 import color from "colors";
 import connectDB from "./config/db.js";
 
+import postRoutes from "./routes/posts.js";
+
 dotenv.config();
 
 connectDB();
@@ -14,6 +16,8 @@ const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use("/posts", postRoutes);
 
 //connection with database
 
