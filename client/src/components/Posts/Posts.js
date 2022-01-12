3a0abbb2bc936bf9df9a74c0e4b37a174119dbdc
gardianos <gardianii@hotmail.com>
2022-01-12 12:04:21 +0100

    Post, Posts and Forms styles in frontend

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Post from "./Post/Post";
+import useStyles from "./styles";
+
+const Posts = () => {
+  const classes = useStyles();
+  return;
+  // eslint-disable-next-line no-unreachable
+
+  // eslint-disable-next-line no-unreachable
+  <>
+    <h1>POSTS</h1>
+    <Post />
+    <Post />
+  </>;
+};
+
+export default Posts;
