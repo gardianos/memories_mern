a2992bd0caf05ca9e35b9f6c4014bd000d521168
gardianos <gardianii@hotmail.com>
2022-01-13 17:44:34 +0100

    posts changes and some changes in app

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 
-import memories from "./images/memories.jpg";
-import useStyles from "./styles";
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
+import memories from "./images/memories.jpg";
+import useStyles from "./styles";
 
 const App = () => {
   const classes = useStyles();
@@ -28,12 +28,13 @@ const App = () => {
             justify="space-between"
             alignItems="stretch"
             spacing={3}
-          />
-          <Grid item xs={12} sm={7}>
-            <Posts />
-          </Grid>
-          <Grid item xs={12} sm={4}>
-            <Form />
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
           </Grid>
         </Container>
       </Grow>
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unreachable */
 import React from "react";
 import Post from "./Post/Post";
 import useStyles from "./styles";
@@ -5,9 +6,6 @@ import useStyles from "./styles";
 const Posts = () => {
   const classes = useStyles();
   return;
-  // eslint-disable-next-line no-unreachable
-
-  // eslint-disable-next-line no-unreachable
   <>
     <h1>POSTS</h1>
     <Post />
