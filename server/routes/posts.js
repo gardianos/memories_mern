a3748a5b8dba20522cf728033104bd5d775d7316
gardianos <gardianii@hotmail.com>
2022-02-10 19:07:26 +0100

    like post implemented

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -6,6 +6,7 @@ import {
   getPost,
   updatePost,
   deletePost,
+  likePost,
 } from "../controllers/posts.js";
 
 const router = express.Router();
@@ -15,5 +16,6 @@ router.post("/", createPost);
 router.get("/:id", getPost);
 router.patch("/:id", updatePost);
 router.delete("/:id", deletePost);
+router.patch("/:id/likePost", likePost);
 
 export default router;
