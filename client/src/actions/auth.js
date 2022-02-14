175e96b8ba44d44b3ffa8d6a5021a5715e51455f
gardianos <gardianii@hotmail.com>
2022-02-14 19:53:32 +0100

    signin and singup in client finished

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -4,6 +4,8 @@ import * as api from "../api/index.js";
 export const signin = (formData, history) => async (dispatch) => {
   try {
     //login user
+    const { data } = await api.signIn(formData);
+    dispatch({ type: AUTH, data });
     history.push("/");
   } catch (error) {
     console.log(error);
@@ -13,6 +15,8 @@ export const signin = (formData, history) => async (dispatch) => {
 export const signup = (formData, history) => async (dispatch) => {
   try {
     //register user
+    const { data } = await api.signUp(formData);
+    dispatch({ type: AUTH, data });
     history.push("/");
   } catch (error) {
     console.log(error);
